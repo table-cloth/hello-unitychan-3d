af5fa98732f209cca0eeb61096049bf8ba55be97
Minami <table.cloth.03@gmail.com>
2016-11-27 23:30:09 +0700

    add jump by click

---
+++ Assets/Scripts/RunUnityChanController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunUnityChanController : MonoBehaviour {
+
+    [SerializeField]
+    private UnityChanController unityChanController;
+
+    void Update()
+    {
+        // Right click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.transform.gameObject.tag.Contains("UnityChan"))
+                {
+                    this.unityChanController.OnTapped();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UnityChanController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanController : MonoBehaviour {
+
+    /// <summary>
+    /// Raises the tapped event.
+    /// </summary>
+    public void OnTapped()
+    {
+        this.GetComponent<Animator>().SetTrigger("Jump");
+    }
+
+    /// <summary>
+    /// Called on the call change face.
+    /// </summary>
+    public void OnCallChangeFace()
+    {
+        // Do nothing for now
+    }
+
+}
